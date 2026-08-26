697b8968f67440ecb8974df1701c6a821e00be68
Hector Ramirez <67031386+hectorlil48@users.noreply.github.com>
2026-08-26 18:32:47 -0500

    Added the HTML for product description and the logic for the carousel.

---
+++ script.js
@@ -0,0 +1,42 @@
+let slidePosition = 0;
+const slides = document.getElementsByClassName("carousel__item");
+const totalSlides = slides.length;
+
+document
+  .querySelector(".carousel__prev")
+  .addEventListener("click", function () {
+    moveToPrevSlide();
+  });
+
+document
+  .querySelector(".carousel__next")
+  .addEventListener("click", function () {
+    moveToNextSlide();
+  });
+
+function updateSlidePosition() {
+  for (let slide of slides) {
+    slide.classList.remove("carousel__item-visible");
+    slide.classList.add("carousel__item-hidden");
+  }
+
+  slides[slidePosition].classList.add("carousel__item-visible");
+}
+
+function moveToNextSlide() {
+  if (slidePosition === totalSlides - 1) {
+    slidePosition = 0;
+  } else {
+    slidePosition++;
+  }
+  updateSlidePosition();
+}
+
+function moveToPrevSlide() {
+  if (slidePosition === 0) {
+    slidePosition = totalSlides - 1;
+  } else {
+    slidePosition--;
+  }
+  updateSlidePosition();
+}
