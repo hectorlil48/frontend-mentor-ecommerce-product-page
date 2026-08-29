6b9e5375f31fca5513846a6b0f013e073c52132d
Hector Ramirez <67031386+hectorlil48@users.noreply.github.com>
2026-08-29 12:24:58 -0500

    Added the logic for increasing and decreasing the count.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Carousel Logic, for mobile and tablet
 let slidePosition = 0;
 const slides = document.getElementsByClassName("carousel__item");
 const totalSlides = slides.length;
@@ -39,4 +40,40 @@ function moveToPrevSlide() {
     slidePosition--;
   }
   updateSlidePosition();
+}
+
+// Product Quantity and add to cart logic
+let quantity = 0;
+let count = document.querySelector(".product__quantity-value");
+
+const priceText = document.querySelector(
+  ".product__discount-price",
+).textContent;
+const price = parseFloat(priceText.replace("$", ""));
+console.log(price);
+
+document
+  .querySelector(".product__decrease")
+  .addEventListener("click", function () {
+    decreaseCount();
+  });
+
+document
+  .querySelector(".product__increase")
+  .addEventListener("click", function () {
+    increaseCount();
+  });
+
+function decreaseCount() {
+  if (quantity === 0) {
+  } else {
+    quantity--;
+  }
+
+  count.textContent = quantity;
+}
+
+function increaseCount() {
+  quantity++;
+  count.textContent = quantity;
 }
